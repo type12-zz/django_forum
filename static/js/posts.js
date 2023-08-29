420cee7cf8414d69346462b56353fa5c8f9ea9f7
Joshua Moten <joshuam36265@gmail.com>
2023-08-29 15:21:53 -0500

    added function for likes

--- static/js/posts.js
+++ static/js/posts.js
@@ -15,5 +15,20 @@ menuIcons.forEach(icon => {
   });
 });
 
-const likeIcons = document.querySelectorAll('.like_icon');
+const likeIcons = document.querySelectorAll('.like-icon');
+
+likeIcons.forEach(likeIcon => {
+  let numOfLikes = 0;
+  let hasBeenLiked = false;
+  const likeNumSpan = likeIcon.nextElementSibling;
+  likeIcon.addEventListener('click', () => {
+    let numOfLikes = parseInt(likeNumSpan.textContent) || 0;
+    numOfLikes++;
+    likeNumSpan.textContent = numOfLikes;
+    if(!hasBeenLiked){
+      likeIcon.src ='static/img/filled_heart.png'; //set the new src of the image
+      hasBeenLiked = true;
+    }
+  });
+});
 
